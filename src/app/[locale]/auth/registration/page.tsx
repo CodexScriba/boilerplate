b4d41f11be43dfe1debad1aa370641d8eca79eca
daniel0123456789 <daniel0123456789@gmail.com>
2025-03-01 00:10:48 +0000

    added registration page

---
+++ src/app/[locale]/auth/registration/page.tsx
@@ -0,0 +1,11 @@
+// app/page.tsx
+
+import RegistrationCard from "../components/RegistrationCard";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <RegistrationCard />
+    </main>
+  )
+}
